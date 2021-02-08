e958a85a534b5017a6de258120b0701dd646f181
Hongze Yu <hongzeyu@gmail.com>
2021-02-08 13:12:16 -0800

    week of 2/8 update_Basic Homescreen & Fan functionality

--- Assets/Scripts/SceneControlls.cs
+++ Assets/Scripts/SceneControlls.cs
@@ -14,4 +14,20 @@ public class SceneControlls : MonoBehaviour
     {
         GameObject.FindGameObjectWithTag("PlayerBall").transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
     }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("New Scene");
+    }
+
+    public void LoadHome()
+    {
+        SceneManager.LoadScene("HomeScreen");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
